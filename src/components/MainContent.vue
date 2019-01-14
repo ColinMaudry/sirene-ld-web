73a31b0c79d4336c8c866e6220268d3e3841bc18
Colin Maudry <colin@maudry.com>
2019-01-14 15:33:48 +0100

    Thème float dark adapté

---
+++ src/components/MainContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="main-content">
+    <!--page header start-->
+    <div class="page-header">
+      <div class="row">
+        <div class="col-sm-6">
+          <h4>Présentation</h4>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <top-bar/>
+    <left-bar/>
+    <main-content/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TopBar from "@/components/TopBar.vue";
+import LeftBar from "@/components/LeftBar.vue";
+import MainContent from "@/components/MainContent.vue";
+
+export default {
+  name: "home",
+  components: {
+    TopBar,
+    LeftBar,
+    MainContent
+  }
+};
+</script>

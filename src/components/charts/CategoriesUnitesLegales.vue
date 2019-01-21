fd2d6a7a93919dc6721e72851b948907b53b6422
Colin Maudry <colin@maudry.com>
2019-01-21 17:25:43 +0100

    Ajout de graphiques avec c3

---
+++ src/components/charts/CategoriesUnitesLegales.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="categories">
+    <vue-c3 :handler="handler"></vue-c3>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import VueC3 from "vue-c3";
+export default {
+  components: {
+    VueC3
+  },
+  data() {
+    return {
+      handler: new Vue()
+    };
+  },
+  mounted() {
+    const options = {
+      data: {
+        url: "/data/sirene/categories-unites-legales.csv",
+        type: "pie"
+      }
+    };
+    this.handler.$emit("init", options);
+  }
+};
+</script>
+
+<style scoped>
+.categories {
+  height: 150px;
+}
+</style>

bae2822b3b1a86e925353abf0b95bda1e67e27cb
Colin Maudry <colin@maudry.com>
2019-02-08 19:03:25 +0100

    Création d'une page dédiée au DECP, mention dans la page de présentation

---
+++ src/views/MarchesPublics.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="page-header">
+      <div class="row">
+        <div class="col-sm-6">
+          <h4>Données essentielles de la commande publique (DECP)</h4>
+          <p>
+            Plus d'informations sur
+            <a
+              href="https://github.com/colinmaudry/decp-json"
+            >la page de développement du projet</a>.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>

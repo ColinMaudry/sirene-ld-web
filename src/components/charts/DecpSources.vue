643b3494fd711335bedd5234127fabf47873c19c
Colin Maudry <colin@maudry.com>
2019-02-11 20:12:30 +0100

    Liste des DECP sources avec nombre de marchés

--- src/components/charts/DecpSources.vue
+++ src/components/charts/DecpSources.vue
@@ -1,18 +1,22 @@
 <template>
-  <div>
-    <vuetable
-      api-url="http://localhost:4444/api/decp/sources"
-      :fields="fields"
-      ref="vuetable"
-      @vuetable:pagination-data="onPaginationData"
-    ></vuetable>
-    <vuetable-pagination ref="pagination" @vuetable-pagination:change-page="onChangePage"></vuetable-pagination>
+  <div class="panel panel-default">
+    <div class="panel-heading">Sources de données essentielles utilisées</div>
+    <div class="panel-body">
+      <vuetable
+        :api-url="apiServer + '/api/decp/sources'"
+        :fields="fields"
+        ref="vuetable"
+        @vuetable:pagination-data="onPaginationData"
+      ></vuetable>
+      <vuetable-pagination ref="pagination" @vuetable-pagination:change-page="onChangePage"></vuetable-pagination>
+    </div>
   </div>
 </template>
 
 
 <script>
 import Vuetable from "vuetable-2";
+import axios from "axios";
 //import VuetablePagination from "@/components/VueTablePagination/VuetablePagination";
 export default {
   components: {
@@ -27,22 +31,52 @@ export default {
         "description",
         {
           name: "url",
-          title: "Page Web",
+          title: "",
           callback: "toHref"
+        },
+        {
+          name: "code",
+          title: "Nombre de marchés",
+          callback: "getNumMarchesPerSource"
         }
-      ]
+      ],
+      marches: [],
+      sourcesApi: this.$store.apiServer + "/api/decp/sources"
     };
+  },
+  computed: {
+    apiServer() {
+      return process.env.VUE_APP_API_SERVER;
+    }
   },
   methods: {
     toHref(url) {
-      return '<a target="_blank" href="' + url + '">' + url + "</a>";
+      return '<a target="_blank" href="' + url + "\">Plus d'infos</a>";
     },
     onPaginationData(paginationData) {
       this.$refs.pagination.setPaginationData(paginationData);
     },
     onChangePage(page) {
       this.$refs.vuetable.changePage(page);
+    },
+    getNumMarchesPerSource(value) {
+      var filtered = this.marches.filter(function(marche) {
+        return marche.source === value;
+      });
+      console.log;
+      return filtered.length;
     }
+  },
+  created() {
+    axios
+      .get(this.apiServer + "/api/decp/marches")
+      .then(response => {
+        this.marches = response.data;
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
   }
 };
 </script>
